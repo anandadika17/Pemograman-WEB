0813a0bbbdd690042297751881399bd14f9df1d1
anandadika17 <147617680+anandadika17@users.noreply.github.com>
2024-03-28 14:21:08 +0700

    Add files via upload

---
+++ PTM1/Pertemuan-1_Latihan-1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 1 - Latihan Soal 1</title>
+    <style>
+        .tengah{
+            margin-left: 450px;
+        }
+    </style>
+</head>
+<body>
+    <?php 
+    print "<pre>";
+    print "<center><font size=14 color='red'><b>BIODATA</b></font></center><br>";
+    print "<font size=12 color='blue' class='tengah'> Nama : <b>Ananda Putra Dika</b> </font><br>";
+    print "<font size=12 color='blue' class='tengah'> Email : tuantak20an@gmail.com </font><br>";
+    print "<font size=12 color='blue' class='tengah'> Jurusan : Teknik Informatika </font><br>";
+    print "<font size=12 color='blue' class='tengah'> Hoby : Travelling </font><br>";
+    ?>
+</body>
+</html>
---
+++ PTM1/Pertemuan-1_Latihan-2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 1 - Latihan Soal 2</title>
+    <style>
+        h1 {
+            color: #333;
+        }
+        
+        p {
+            color: #666;
+            font-size: 18.5px;
+            line-height: 1.5;
+        }
+    </style>
+</head>
+<body style='font-family: Arial, sans-serif; background-color: #f4f4f4; text-align: center;'>
+<div class="container" style='max-width: 600px; margin: 100px auto; padding: 20px; background-color: #fff; border-radius: 8px; box-shadow: 0 1px 20px;'>
+    <?php
+        $kesan_pertama = "Perkenalkan nama saya Ananda Putra Dika dengan NIM 211011400857 dari kelas 06TPLM004, saya seorang Mahasiswa Semester 6 di salah satu dari banyaknya Universitas yang ada di Indonesia. Saat pertama kali belajar PHP, saya merasa sangat tertarik dengan kemampuannya untuk membuat halaman web dinamis. Saya terkesan dengan kekuatan bahasa ini dalam mengolah data, berinteraksi dengan pengguna, dan membuat situs web yang lebih interaktif.";
+
+        echo "<h1>Kesan Pertama Belajar PHP</h1><hr>";
+        echo "<p>$kesan_pertama</p>";
+        ?>
+    </div>
+</body>
+</html>
