fbdfe090c8d004a3cac341806d36d519727a8413
anandadika17 <147617680+anandadika17@users.noreply.github.com>
2024-06-26 21:34:58 +0700

    Delete PTM18/Web sederhana tanpa framework atau cms/myapp directory

--- PTM18/Web sederhana tanpa framework atau cms/myapp/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Config file to store database connection parameters
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'myapp_db');
-?>
